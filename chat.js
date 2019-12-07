8b7fc17918013cb07c2430e2c727a313a766d739
waynemortenson <waynemortenson@gmail.com>
2019-12-06 21:14:03 -0600

    basic messaging added

--- chat.js
+++ chat.js
@@ -4,12 +4,17 @@ const socketio = require("socket.io");
 
 app.use(express.static(__dirname + "/public"));
 
-const expressServer = app.listen(9000, ()=> console.log('Connected to server..'));
+const expressServer = app.listen(9000, () =>
+  console.log("Connected to server..")
+);
 
 const io = socketio(expressServer);
 io.on("connection", socket => {
   socket.emit("messageFromServer", { data: "Welcome to the server" });
-  socket.on("dataToServer", dataFromClient => {
+  socket.on("dataFromClient", dataFromClient => {
     console.log(dataFromClient);
+  });
+  socket.on("newMessageToServer", msg => {
+    io.emit("messageToClients", {text: msg.text});
   });
 });
